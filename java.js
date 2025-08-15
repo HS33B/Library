5ebf04835212f72e7d167f7cc9c1e5b448498dcc
Haseeb Zafar <170537518+HS33B@users.noreply.github.com>
2025-08-15 19:04:25 +0500

    Update java.js

--- java.js
+++ java.js
@@ -19,66 +19,50 @@ but1.addEventListener('click', () => {
 but3.addEventListener('click', () => {
     myDialog.close();
 })
-
-let titleVal = '';
-let authorVal = '';
-let pagesVal = '';
-let statusVal = '';
-let index = 0;
-let indexVal;
-let books;
-
+let library = [];
+let ind = 0;
 but2.addEventListener('click', () => {
+    let titleVal = '';
+    let authorVal = '';
+    let pagesVal = '';
+    let statVal = '';
     if (!title.value == '' && !author.value == '' && !pages.value == '') {
         titleVal = title.value;
         authorVal = author.value;
         pagesVal = pages.value;
-        if(checkbox.checked){
-            statusVal = 'Read';
+        if (checkbox.checked) {
+            statVal = 'Read';
         }
-        else{
-            statusVal = 'Unread';
+        else {
+            statVal = 'Unread';
         }
-        indexVal = index;
-        index++;
-        bookToLibrary();
         myDialog.close();
         title.value = '';
         author.value = '';
         pages.value = '';
         checkbox.checked = false;
+        const book = new Book(titleVal, authorVal, pagesVal, statVal, ind);
+        library.push(book);
+        ind++;
+        displayBooks();
     }
     else{
         alert('Kindly Fill All Feilds!')
     }
 })
-
-let myLibrary = [];
-function Book(title, author, pages, status, index) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-    this.index  = index;
-}
-Book.prototype.toggleFunc = function() {
-    if (this.status == 'Read') {
-        this.status = 'Unread';
+class Book {
+    constructor(title, author, pages, stat, ind) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.stat = stat;
+        this.ind = ind;
     }
-    else{
-        this.status = 'Read';
-    }
-    displayBooks();
-}
-function bookToLibrary() {
-    books = new Book(titleVal, authorVal, pagesVal, statusVal, indexVal);
-    myLibrary.push(books);
-    displayBooks();
 }
 function displayBooks() {
     cards.innerHTML = '';
-    for (let i=0; i<myLibrary.length; i++) {
-        
+    for (let i = 0; i < library.length; i++) {
+
         const card = document.createElement('div');
         card.classList.add('card');
         const titleDiv = document.createElement('div');
@@ -89,10 +73,10 @@ function displayBooks() {
         authorDiv.classList.add('authorDiv');
         pageDiv.classList.add('pageDiv');
         statusDiv.classList.add('statusDiv');
-        titleDiv.textContent = myLibrary[i].title;
-        authorDiv.textContent = '- Author: ' + myLibrary[i].author;
-        pageDiv.textContent = '- No. of Pages: ' + myLibrary[i].pages;
-        statusDiv.textContent = '- Status: ' + myLibrary[i].status;
+        titleDiv.textContent = library[i].title;
+        authorDiv.textContent = '- Author: ' + library[i].author;
+        pageDiv.textContent = '- No. of Pages: ' + library[i].pages;
+        statusDiv.textContent = '- Status: ' + library[i].stat;
 
         const butHolder = document.createElement('div');
         butHolder.classList.add('butHolder');
@@ -104,7 +88,7 @@ function displayBooks() {
         toggleBut.setAttribute('data-index', i);
         toggleBut.addEventListener('click', (e) => {
             let y = e.target.getAttribute('data-index');
-            myLibrary[y].toggleFunc();
+            library[y].toggleFunc();
         })
 
 
@@ -114,7 +98,7 @@ function displayBooks() {
         delBut.textContent = 'Delete';
         delBut.addEventListener('click', (e) => {
             let x = e.target.getAttribute('data-index');
-            myLibrary.splice(x, 1);
+            library.splice(x, 1);
             displayBooks();
         })
 
@@ -127,4 +111,13 @@ function displayBooks() {
         butHolder.appendChild(toggleBut);
         butHolder.appendChild(delBut);
     }
+}
+Book.prototype.toggleFunc = function () {
+    if (this.stat == 'Read') {
+        this.stat = 'Unread';
+    }
+    else {
+        this.stat = 'Read';
+    }
+    displayBooks();
 }
